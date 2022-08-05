402a59fc439c6f73f982eb0aeced810dd7b36fd8
samuelgcabral <samuel.gc1406@gmail.com>
2022-08-05 17:58:59 -0300

    req3

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class List extends Component {
   render() {
     return (
       <div>
-        {/* <input type="text" /> */}
+        <Link data-testid="shopping-cart-button" to="./shoppingcart">
+          <button type="button">
+            Carrinho de Compras
+          </button>
+        </Link>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
