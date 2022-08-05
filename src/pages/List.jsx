53f9c47a3eb2b35b94bb9f37b8d4b112a97a48e1
André Pestana Silva <atlas.ifpa@gmail.com>
2022-08-05 19:10:24 -0300

    04- listando categorias

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.requestApi();
+  }
+
+  requestApi = async () => {
+    const requestList = await getCategories();
+    this.setState({ categories: requestList });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <div>
-        <Link data-testid="shopping-cart-button" to="./shoppingcart">
-          <button type="button">
-            Shopping Cart
-          </button>
-        </Link>
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-      </div>
+      <main>
+        <div>
+          <Link data-testid="shopping-cart-button" to="./shoppingcart">
+            <button type="button">
+              Shopping Cart
+            </button>
+          </Link>
+          <h1 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        </div>
+        <form>
+          { categories.map((categorie) => (
+            <label key={ categorie.id } data-testid="category" htmlFor="radio">
+              <input type="radio" name="radio" value={ categorie.name } />
+              { categorie.name }
+            </label>
+          )) }
+        </form>
+      </main>
     );
   }
 }
