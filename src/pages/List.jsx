80ef8d5b6556ae9db12c1caa1147d91699fbf0bb
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-08-05 21:04:04 -0300

    req5

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class List extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputText: '',
+    };
+  }
+
+  // componentDidMount() {
+  //   this.fetchApi();
+  // }
+
+  onHandleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value }, () => ({
+      inputText: value,
+      filteredProducts: [],
+    }));
+  }
+
+  fetchApi = async () => {
+    const { inputText } = this.state;
+    const returnFetch = await getProductsFromCategoryAndQuery(inputText);
+    this.setState({
+      filteredProducts: returnFetch.results,
+    });
+  }
+
   render() {
+    const { inputText, filteredProducts } = this.state;
+    console.log(filteredProducts);
     return (
       <div>
         <Link data-testid="shopping-cart-button" to="./shoppingcart">
@@ -13,6 +44,31 @@ export default class List extends Component {
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
+        <input
+          type="text"
+          name="inputText"
+          value={ inputText }
+          data-testid="query-input"
+          onChange={ this.onHandleChange }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.fetchApi }
+        >
+          Chamar API
+        </button>
+        {filteredProducts !== undefined && filteredProducts.length !== 0
+          ? filteredProducts.map((product) => (
+            <section key={ product.id } data-testid="product">
+              <h3>{product.title}</h3>
+              <img
+                src={ product.thumbnail }
+                alt={ product.title }
+              />
+              <h4>{ product.price }</h4>
+            </section>
+          )) : <p>Nenhum produto foi encontrado</p>}
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,15 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId || query}`);
+  let URL = '';
+  if (categoryId) {
+    URL = `https://api.mercadolibre.com/sites/MLB/search?q=${categoryId}`;
+  } else if (query) {
+    URL = `https://api.mercadolibre.com/sites/MLB/search?category=${query}`;
+  } else {
+    URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  }
+  const request = await fetch(URL);
   const response = await request.json();
   return response;
 }
