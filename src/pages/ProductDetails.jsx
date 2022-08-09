e24112628b8ae4a6d8015dcd988b6c67a69968b2
André Pestana Silva <atlas.ifpa@gmail.com>
2022-08-08 22:03:32 -0300

    11- Não finalizado

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -5,6 +5,7 @@ import { getProductFromId } from '../services/api';
 
 class ProductDetails extends Component {
   state = {
+    comments: JSON.parse(localStorage.getItem('Formulario')) || [],
     product: [],
     cartList: [],
   }
@@ -35,8 +36,27 @@ class ProductDetails extends Component {
     }));
   }
 
+  handleChange = ({ target: { name, value, type, checked } }) => {
+    const newValue = type === 'checked' ? checked : value;
+    this.setState({
+      [name]: newValue,
+    });
+  };
+
+  handleClickSubmit = (event) => {
+    console.log(event.target);
+    const { email, rating, textarea } = this.state;
+    const object = { email, rating, textarea };
+    this.setState((prevState) => ({
+      comments: [...prevState.comments, object],
+    }), () => {
+      const { comments } = this.state;
+      localStorage.setItem('Formulario', JSON.stringify(comments));
+    });
+  };
+
   render() {
-    const { product, cartList } = this.state;
+    const { product, cartList, comments } = this.state;
     return (
       <div>
         <h1 data-testid="product-detail-name">{ product.title }</h1>
@@ -66,6 +86,84 @@ class ProductDetails extends Component {
             Ir ao carrinho
           </button>
         </Link>
+        <form>
+          <input
+            type="email"
+            name="email"
+            data-testid="product-detail-email"
+            onChange={ this.handleChange }
+            required
+          />
+          <label htmlFor="radio">
+            <input
+              type="radio"
+              name="rating"
+              value="1"
+              data-testid="1-rating"
+              onChange={ this.handleChange }
+              required
+            />
+          </label>
+          <label htmlFor="radio">
+            <input
+              type="radio"
+              name="rating"
+              value="2"
+              data-testid="2-rating"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="radio">
+            <input
+              type="radio"
+              name="rating"
+              value="3"
+              data-testid="3-rating"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="radio">
+            <input
+              type="radio"
+              name="rating"
+              value="4"
+              data-testid="4-rating"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="radio">
+            <input
+              type="radio"
+              name="rating"
+              value="5"
+              data-testid="5-rating"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <textarea
+            data-testid="product-detail-evaluation"
+            id=""
+            cols="30"
+            rows="10"
+            name="textarea"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="submit-review-btn"
+            type="submit"
+            id={ product.id }
+            onClick={ this.handleClickSubmit }
+          >
+            Avaliar
+          </button>
+        </form>
+        { comments.map((comment, index) => (
+          <section key={ index }>
+            <h1>{ comment.email }</h1>
+            <h2>{ comment.textarea }</h2>
+            <h3>{ comment.rating }</h3>
+          </section>
+        )) }
       </div>
     );
   }
